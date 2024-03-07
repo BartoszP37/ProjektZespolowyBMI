042fb937801da5826d64e43205ad17f72d3d2e98
BartoszP37 <103176742+BartoszP37@users.noreply.github.com>
2024-03-07 09:36:43 +0100

    Add files via upload
    Plik SpringBoot

---
+++ ProjektZespolowy/src/test/java/com/example/ProjektZespolowy/ProjektZespolowyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjektZespolowy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektZespolowyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
