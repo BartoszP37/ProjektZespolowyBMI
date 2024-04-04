ef583784449f7c6c51af61f10ee8179424b6e8fa
BartoszP37 <103176742+BartoszP37@users.noreply.github.com>
2024-04-04 09:51:11 +0200

    Usprawnienie funkcjonalności

--- ProjektZespolowy/src/main/java/com/example/ProjektZespolowy/controller/LiczbaPierwszaController.java
+++ ProjektZespolowy/src/main/java/com/example/ProjektZespolowy/controller/LiczbaPierwszaController.java
@@ -13,18 +13,25 @@ public class LiczbaPierwszaController {
         return "liczbaPierwsza";
         }
     @PostMapping("/pierwsza")
-    public String pierwsza(@RequestParam("liczba") int liczba, Model model){
-        boolean pierwsza=true;
-        while (pierwsza){
-            for(int i=0; i>=liczba/2; i++){
-                if(liczba%i==0){
-                    pierwsza=false;
-                    model.addAttribute("info",liczba + " nie jest liczbą pierwszą.");
+    public String pierwsza(@RequestParam("liczba") int liczba, Model model) {
+        boolean pierwsza = true;
+
+        if (liczba <= 1) {
+            pierwsza = false;
+        } else {
+            for (int i = 2; i <= liczba / 2; i++) {
+                if (liczba % i == 0) {
+                    pierwsza = false;
+                    break;
                 }
             }
         }
-        model.addAttribute("info",liczba + " jest liczbą pierwszą.");
 
+        if (pierwsza) {
+            model.addAttribute("info", liczba + " jest liczbą pierwszą.");
+        } else {
+            model.addAttribute("info", liczba + " nie jest liczbą pierwszą.");
+        }
 
         return "liczbaPierwsza";
     }
